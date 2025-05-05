eb566892663fba5e25a546bb0714b93eb0aca2c3
peppone-choi <peppone.choi@gmail.com>
2025-05-06 01:57:21 +0900

    여러 모듈 추가

---
+++ cron/src/main/java/com/pepponechoi/cron/CronApplication.java
@@ -0,0 +1,13 @@
+package com.pepponechoi.cron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CronApplication.class, args);
+    }
+
+}
